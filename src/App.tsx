fc7329284f6174cd202ac585525b8ad915afb34b
Lucas Capdevila <lcapdevila60@gmail.com>
2025-08-16 19:25:02 -0300

    Se añaden estilos globales

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import MenuDisplay from './components/MenuExample';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-surface">
       <MenuDisplay />
     </div>
   )
